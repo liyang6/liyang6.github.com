c8fba1d89a939ec40de1e4d2d830f3b7a42c2476
liyang <liyang368@126.com>
2017-01-12 17:29:06 +0800

    修改配置文件

--- js/config.js
+++ js/config.js
@@ -1,15 +1,16 @@
 //基本设置（像素比、Html字体大小）
-function config(){
+(function (doc, win) {
     //动态设置像素比
-    var oPixelRatio = 1 / window.devicePixelRatio;
-    document.write('<meta name="viewport" content="width=device-width,initial-scale='+oPixelRatio+',minimum-scale='+oPixelRatio+',maximum-scale='+oPixelRatio+',user-scalable=no" />');
+    var oPixelRatio = 1 / win.devicePixelRatio;
+    doc.write('<meta name="viewport" content="width=device-width,initial-scale='+oPixelRatio+',minimum-scale='+oPixelRatio+',maximum-scale='+oPixelRatio+',user-scalable=no" />');
     //设置字体大小
     var setHtmlSize=function (){
-        var oHtml = document.getElementsByTagName('html')[0];
+        var oHtml = doc.getElementsByTagName('html')[0];
         oHtml.style.fontSize =20*oHtml.getBoundingClientRect().width / 320 +"px";
     };
-    setHtmlSize();
-    window.addEventListener("resize", setHtmlSize, false);
-    window.addEventListener("orientationchange", setHtmlSize, false);
-}
-config();
+    var resizeEvt = 'orientationchange' in win ? 'orientationchange' : 'resize',
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, setHtmlSize, false);
+    doc.addEventListener('DOMContentLoaded',setHtmlSize, false);
+    //
+})(document, window);
