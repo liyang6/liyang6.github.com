bca5c56645ea26c8dd0a9098ec986f1c21b673d2
liyang <liyang368@126.com>
2017-01-19 17:53:46 +0800

    修改基础文件

--- public/m_config.js
+++ public/m_config.js
@@ -1,16 +1,42 @@
 //基本设置（像素比、Html字体大小）
 (function (doc, win) {
-    //动态设置像素比
-    var oPixelRatio = 1 / win.devicePixelRatio;
-    doc.write('<meta name="viewport" content="width=device-width,initial-scale='+oPixelRatio+',minimum-scale='+oPixelRatio+',maximum-scale='+oPixelRatio+',user-scalable=no" />');
-    //设置字体大小
-    var setHtmlSize=function (){
-        var oHtml = doc.getElementsByTagName('html')[0];
-        oHtml.style.fontSize =20*oHtml.getBoundingClientRect().width / 320 +"px";//默认640设计图，750设计图，只需要修改成375即可
-    };
-    var resizeEvt = 'orientationchange' in win ? 'orientationchange' : 'resize';
-    if( !doc.addEventListener) return;
-    win.addEventListener(resizeEvt, setHtmlSize, false);
-    doc.addEventListener('DOMContentLoaded',setHtmlSize, false);
-    //
+    var oBaseSet={
+            setPixelRatio:function (){//动态设置像素比
+                var oPixelRatio = 1 / win.devicePixelRatio;
+                doc.write('<meta name="viewport" content="width=device-width,initial-scale='+oPixelRatio+',minimum-scale='+oPixelRatio+',maximum-scale='+oPixelRatio+',user-scalable=no" />');
+            },
+            setRem:function (ImgW,ImgM){//设置字体大小(默认20px,640的2倍图)
+                ImgM=ImgM || 2;
+                ImgW=(ImgW || 640)/ImgM;
+                var setHtmlSize=function (){
+                    var oHtml = doc.getElementsByTagName('html')[0];
+                    oHtml.style.fontSize =10*oHtml.getBoundingClientRect().width / ImgW +"px";
+                };
+                var resizeEvt = 'orientationchange' in win ? 'orientationchange' : 'resize';
+                if( !doc.addEventListener) return;
+                win.addEventListener(resizeEvt, setHtmlSize, false);
+                doc.addEventListener('DOMContentLoaded',setHtmlSize, false);
+            },
+            preventHref:function (){//给所有的a动态加链接(防止误触)
+                var fnPreventHref=function (){
+                    var aBtn = document.querySelectorAll('a');
+                    for(var i=0; i<aBtn.length; i++){
+                        aBtn[i].addEventListener("touchmove", function(){
+                            this.isMove = true;
+                        }, false);
+                        
+                        aBtn[i].addEventListener("touchend", function(){
+                            if(!this.isMove){
+                                window.location.href = this.href;
+                            }
+                            this.isMove = false;
+                        }, false);
+                    }
+                };
+                doc.addEventListener('DOMContentLoaded',fnPreventHref, false);
+            }
+    }
+    oBaseSet.setPixelRatio();//动态设置像素比
+    oBaseSet.setRem();//设置字体大小
+    oBaseSet.preventHref();//给所有的a动态加链接(防止误触)
 })(document, window);
